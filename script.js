fc88ab01ed075d06133a5a96d45692faa1be1108
Jiyeon31 <zhdzhddlx@gmail.com>
2022-04-03 05:08:13 -0400

    edited bugs

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const saveBtn = $(".saveBtn");
 $("#currentDay").text(moment().format('dddd, MMM Do'));
 
 function hourColors () {
@@ -17,15 +18,20 @@ function hourColors () {
 });
 };
 
+saveBtn.on("click", function () {
+    const currTime = $(this).siblings(".span").text();
+    const plans = $(this).siblings("#inputPlan").val();
+    localStorage.setItem(currTime, plans);
+});
 
-const saveBtn = $(".saveBtn");
-    for (var i = 0; i < saveBtn.length; i++) {
-        saveBtn[i].addEventListener("click", function () {
-            const currTime = parseInt($(".span").attr("id"));
-            const plans = document.getElementById("inputPlan").value;
-            localStorage.setItem(currTime, plans);
-        });
+function keepPlans () {
+    $(".span").each(function() {
+    const currentTime = $(this).text();
+    const savedPlans = localStorage.getItem(currentTime);
+    $(this).siblings("#inputPlan").val(savedPlans);
+})
 };
 
 hourColors();
+keepPlans();
 
